80ce17cd7390c0d9f20063a659dd4afce772bf82
chimnayajith <chinmayajith30@gmail.com>
2026-04-13 15:06:35 +0530

    add zsh config to flake

--- home/chimnayyyy.nix
+++ home/chimnayyyy.nix
@@ -14,11 +14,40 @@
     };
   };
 
-  programs.zsh.enable = true;
+  programs.zsh = {
+    enable = true;
+
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    oh-my-zsh = {
+      enable = true;
+      theme = "robbyrussell";
+      plugins = [ "git" ];
+    };
+
+    shellAliases = {
+      # you can add your aliases here later
+    };
+
+    initExtra = ''
+    # Powerlevel10k instant prompt
+    if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+      source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+    fi
+
+    export ZSH="$HOME/.oh-my-zsh"
+
+    eval "$(zoxide init zsh --cmd cd)"
+  '';
+  };
+
 
   home.packages = with pkgs; [
     neovim
     vim
+    zoxide
     firefox-unwrapped
     vscode
     discord
